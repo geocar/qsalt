96b4305e79abcda8d7ad55faa9b3bfca8b0c8885
Geo Carncross <geocar@gmail.com>
2015-12-03 08:45:31 +0000

    use KG instead of KC

--- qsalt.c
+++ qsalt.c
@@ -8,22 +8,21 @@
 
 typedef unsigned long long j;
 
-void randombytes(unsigned char *b,j n) {
-  I f,r;while(n>0){r=read(f=open("/dev/urandom",O_RDONLY),b,n);close(f);if(r>0)b+=r,n-=r;}
-}
+void randombytes(unsigned char *b,j n){I f,r;while(n>0){r=read(f=open("/dev/urandom",O_RDONLY),b,n);close(f);if(r>0)b+=r,n-=r;}}
 
-ZK nonce(K _){G n[32];randombytes(n,32);R kpn(n,32);}//->nonce
+ZK gg(K x){xt=KG;R x;}
+ZK nonce(K _){G n[32];randombytes(n,32);R gg(kpn(n,32));}//->nonce
 
-ZK pkey(K x){G p[32];crypto_box_curve25519xsalsa20poly1305_ref_keypair(p,xG);R kpn(p,32);}//secret->public
+ZK pkey(K x){G p[32];crypto_box_curve25519xsalsa20poly1305_ref_keypair(p,xG);R gg(kpn(p,32));}//secret->public
 ZK pencrypt(K x,K y,K z){K r=ktn(KG,xy->n);crypto_box_curve25519xsalsa20poly1305_ref(kG(r),kG(xy),r->n,kG(xx),kG(y),kG(z));jv(&r,xx);R r;}//(nonce;text);receiverpublic;sendersecret->message
-ZK pdecrypt(K x,K y,K z){K r=ktn(KG,xn-32);if(-1==crypto_box_curve25519xsalsa20poly1305_ref_open(kG(r),xG,xn-32,xG+(xn-32),kG(y),kG(z))){r0(r);R ks("");}R r;}//message;senderpublic;receiversecret->text
+ZK pdecrypt(K x,K y,K z){K r=ktn(KG,xn-32);if(-1==crypto_box_curve25519xsalsa20poly1305_ref_open(kG(r),xG,xn-32,xG+(xn-32),kG(y),kG(z))){r0(r);R gg(ks(""));}R r;}//message;senderpublic;receiversecret->text
 
-ZK newkey(K _){G p[32],s[32];crypto_sign_edwards25519sha512batch_ref_keypair(p,s);R knk(2,kpn(p,32),kpn(s,32),0);}//->(public;secret)
+ZK newkey(K _){G p[32],s[32];crypto_sign_edwards25519sha512batch_ref_keypair(p,s);R knk(2,gg(kpn(p,32)),gg(kpn(s,32)),0);}//->(public;secret)
 ZK sign(K x,K y){K r=ktn(KG,xn+64);j n=r->n;crypto_sign_edwards25519sha512batch_ref(kG(r),&n,xG,xn,kG(y));r->n=n;R r;}//text;secretkey->message
-ZK verify(K x,K y){K r=ktn(KG,xn-64);j n=r->n;if(-1==crypto_sign_edwards25519sha512batch_ref_open(kG(r),&n,xG,xn,kG(y))){r0(r);R ks("");}r->n=n;R r;}//message;publickey->text|null
+ZK verify(K x,K y){K r=ktn(KG,xn-64);j n=r->n;if(-1==crypto_sign_edwards25519sha512batch_ref_open(kG(r),&n,xG,xn,kG(y))){r0(r);R gg(ks(""));}r->n=n;R r;}//message;publickey->text|null
 
 ZK sencrypt(K x,K y){K r=ktn(KG,xy->n);crypto_secretbox_xsalsa20poly1305_ref(kG(r),kG(xy),r->n,kG(xx),kG(y));jv(&r,xx);R r;}//(nonce;text);secret->message
-ZK sdecrypt(K x,K y){K r=ktn(KG,xn-32);if(-1==crypto_secretbox_xsalsa20poly1305_ref_open(kG(r),xG,xn-32,xG+(xn-32),kG(y))){r0(r);R ks("");}R r;}//message;secret->text
+ZK sdecrypt(K x,K y){K r=ktn(KG,xn-32);if(-1==crypto_secretbox_xsalsa20poly1305_ref_open(kG(r),xG,xn-32,xG+(xn-32),kG(y))){r0(r);R gg(ks(""));}R r;}//message;secret->text
 
 //no point is unsing the unauthenticated encryption
 
