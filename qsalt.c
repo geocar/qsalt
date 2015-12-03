64ec9e9af5c9be16f4e54b38c337cc3a951c75f2
Geo Carncross <geocar@gmail.com>
2015-12-03 08:39:29 +0000

    change symbols for compatibility with ref/tweet

--- qsalt.c
+++ qsalt.c
@@ -14,26 +14,28 @@ void randombytes(unsigned char *b,j n) {
 
 ZK nonce(K _){G n[32];randombytes(n,32);R kpn(n,32);}//->nonce
 
-ZK pkey(K x){G p[32];crypto_box_curve25519xsalsa20poly1305_keypair(p,xG);R kpn(p,32);}//secret->public
-ZK pencrypt(K x,K y,K z){K r=ktn(KG,xy->n);crypto_box_curve25519xsalsa20poly1305(kG(r),kG(xy),r->n,kG(xx),kG(y),kG(z));jv(&r,xx);R r;}//(nonce;text);receiverpublic;sendersecret->message
-ZK pdecrypt(K x,K y,K z){K r=ktn(KG,xn-32);if(-1==crypto_box_curve25519xsalsa20poly1305_open(kG(r),xG,xn-32,xG+(xn-32),kG(y),kG(z))){r0(r);R ks("");}R r;}//message;senderpublic;receiversecret->text
+ZK pkey(K x){G p[32];crypto_box_curve25519xsalsa20poly1305_ref_keypair(p,xG);R kpn(p,32);}//secret->public
+ZK pencrypt(K x,K y,K z){K r=ktn(KG,xy->n);crypto_box_curve25519xsalsa20poly1305_ref(kG(r),kG(xy),r->n,kG(xx),kG(y),kG(z));jv(&r,xx);R r;}//(nonce;text);receiverpublic;sendersecret->message
+ZK pdecrypt(K x,K y,K z){K r=ktn(KG,xn-32);if(-1==crypto_box_curve25519xsalsa20poly1305_ref_open(kG(r),xG,xn-32,xG+(xn-32),kG(y),kG(z))){r0(r);R ks("");}R r;}//message;senderpublic;receiversecret->text
 
-ZK newkey(K _){G p[32],s[32];crypto_sign_ed25519_keypair(p,s);R knk(2,kpn(p,32),kpn(s,32),0);}//->(public;secret)
-ZK sign(K x,K y){K r=ktn(KG,xn+64);j n=r->n;crypto_sign_ed25519(kG(r),&n,xG,xn,kG(y));r->n=n;R r;}//text;secretkey->message
-ZK verify(K x,K y){K r=ktn(KG,xn-64);j n=r->n;if(-1==crypto_sign_ed25519_open(kG(r),&n,xG,xn,kG(y))){r0(r);R ks("");}r->n=n;R r;}//message;publickey->text|null
+ZK newkey(K _){G p[32],s[32];crypto_sign_edwards25519sha512batch_ref_keypair(p,s);R knk(2,kpn(p,32),kpn(s,32),0);}//->(public;secret)
+ZK sign(K x,K y){K r=ktn(KG,xn+64);j n=r->n;crypto_sign_edwards25519sha512batch_ref(kG(r),&n,xG,xn,kG(y));r->n=n;R r;}//text;secretkey->message
+ZK verify(K x,K y){K r=ktn(KG,xn-64);j n=r->n;if(-1==crypto_sign_edwards25519sha512batch_ref_open(kG(r),&n,xG,xn,kG(y))){r0(r);R ks("");}r->n=n;R r;}//message;publickey->text|null
 
-ZK sencrypt(K x,K y){K r=ktn(KG,xy->n);crypto_secretbox_xsalsa20poly1305(kG(r),kG(xy),r->n,kG(xx),kG(y));jv(&r,xx);R r;}//(nonce;text);secret->message
-ZK sdecrypt(K x,K y){K r=ktn(KG,xn-32);if(-1==crypto_secretbox_xsalsa20poly1305_open(kG(r),xG,xn-32,xG+(xn-32),kG(y))){r0(r);R ks("");}R r;}//message;secret->text
+ZK sencrypt(K x,K y){K r=ktn(KG,xy->n);crypto_secretbox_xsalsa20poly1305_ref(kG(r),kG(xy),r->n,kG(xx),kG(y));jv(&r,xx);R r;}//(nonce;text);secret->message
+ZK sdecrypt(K x,K y){K r=ktn(KG,xn-32);if(-1==crypto_secretbox_xsalsa20poly1305_ref_open(kG(r),xG,xn-32,xG+(xn-32),kG(y))){r0(r);R ks("");}R r;}//message;secret->text
 
 //no point is unsing the unauthenticated encryption
 
-ZK hash(K x){K r=ktn(KG,64);crypto_hash_sha512(kG(r),xG,xn);R r;}
-ZK cmp16(K x,K y){R kb(crypto_verify_16(xG,kG(y)));}
-ZK cmp32(K x,K y){R kb(crypto_verify_32(xG,kG(y)));}
+ZK hash(K x){K r=ktn(KG,64);crypto_hash_sha512_ref(kG(r),xG,xn);R r;}
+ZK cmp16(K x,K y){R kb(crypto_verify_16_ref(xG,kG(y)));}
+ZK cmp32(K x,K y){R kb(crypto_verify_32_ref(xG,kG(y)));}
 
-K qsalt(K _){
+K qsalt(K x){
   ZS a[]={"nonce","pkey","pencrypt","pdecrypt","newkey","sign","verify","sencrypt","sdecrypt","hash","cmp16","cmp32"};
   ZV*b[]={ nonce , pkey , pencrypt , pdecrypt , newkey , sign , verify , sencrypt , sdecrypt , hash , cmp16 , cmp32 };
   ZI c[]={ 1     , 1    , 3        , 3        , 1      , 2    , 2      , 2        , 2        , 1    , 2     , 2     };
-  ZI n=sizeof(a)/sizeof(S);K x=ktn(KS,n),y=ktn(0,n);DO(n,xS[i]=ss(a[i]);kK(y)[i]=dl(b[i],c[i]));R xD(x,y);
+  ZI n=sizeof(a)/sizeof(S);
+  if(xt==-KS){DO(n,if(!strcmp(a[i],x->s))R dl(b[i],c[i]));R ktn(0,0);}
+  K y;x=ktn(KS,n),y=ktn(0,n);DO(n,xS[i]=ss(a[i]);kK(y)[i]=r1(dl(b[i],c[i])));R xD(x,y);
 }
--- tweetnacl.h
+++ tweetnacl.h
@@ -1,8 +1,8 @@
 #ifndef TWEETNACL_H
 #define TWEETNACL_H
 #define crypto_auth_PRIMITIVE "hmacsha512256"
-#define crypto_auth crypto_auth_hmacsha512256
-#define crypto_auth_verify crypto_auth_hmacsha512256_verify
+#define crypto_auth crypto_auth_hmacsha51225_ref6
+#define crypto_auth_verify crypto_auth_hmacsha512256_ref_verify
 #define crypto_auth_BYTES crypto_auth_hmacsha512256_BYTES
 #define crypto_auth_KEYBYTES crypto_auth_hmacsha512256_KEYBYTES
 #define crypto_auth_IMPLEMENTATION crypto_auth_hmacsha512256_IMPLEMENTATION
@@ -17,9 +17,9 @@ extern int crypto_auth_hmacsha512256_tweet_verify(const unsigned char *,const un
 #define crypto_auth_hmacsha512256_VERSION crypto_auth_hmacsha512256_tweet_VERSION
 #define crypto_auth_hmacsha512256_IMPLEMENTATION "crypto_auth/hmacsha512256/tweet"
 #define crypto_box_PRIMITIVE "curve25519xsalsa20poly1305"
-#define crypto_box crypto_box_curve25519xsalsa20poly1305
-#define crypto_box_open crypto_box_curve25519xsalsa20poly1305_open
-#define crypto_box_keypair crypto_box_curve25519xsalsa20poly1305_keypair
+#define crypto_box crypto_box_curve25519xsalsa20poly1305_ref
+#define crypto_box_open crypto_box_curve25519xsalsa20poly1305_ref_open
+#define crypto_box_keypair crypto_box_curve25519xsalsa20poly1305_ref_keypair
 #define crypto_box_beforenm crypto_box_curve25519xsalsa20poly1305_beforenm
 #define crypto_box_afternm crypto_box_curve25519xsalsa20poly1305_afternm
 #define crypto_box_open_afternm crypto_box_curve25519xsalsa20poly1305_open_afternm
@@ -113,7 +113,7 @@ extern int crypto_hashblocks_sha256_tweet(unsigned char *,const unsigned char *,
 #define crypto_hashblocks_sha256_VERSION crypto_hashblocks_sha256_tweet_VERSION
 #define crypto_hashblocks_sha256_IMPLEMENTATION "crypto_hashblocks/sha256/tweet"
 #define crypto_hash_PRIMITIVE "sha512"
-#define crypto_hash crypto_hash_sha512
+#define crypto_hash crypto_hash_sha512_ref
 #define crypto_hash_BYTES crypto_hash_sha512_BYTES
 #define crypto_hash_IMPLEMENTATION crypto_hash_sha512_IMPLEMENTATION
 #define crypto_hash_VERSION crypto_hash_sha512_VERSION
@@ -167,8 +167,8 @@ extern int crypto_scalarmult_curve25519_tweet_base(unsigned char *,const unsigne
 #define crypto_scalarmult_curve25519_VERSION crypto_scalarmult_curve25519_tweet_VERSION
 #define crypto_scalarmult_curve25519_IMPLEMENTATION "crypto_scalarmult/curve25519/tweet"
 #define crypto_secretbox_PRIMITIVE "xsalsa20poly1305"
-#define crypto_secretbox crypto_secretbox_xsalsa20poly1305
-#define crypto_secretbox_open crypto_secretbox_xsalsa20poly1305_open
+#define crypto_secretbox crypto_secretbox_xsalsa20poly1305_ref
+#define crypto_secretbox_open crypto_secretbox_xsalsa20poly1305_ref_open
 #define crypto_secretbox_KEYBYTES crypto_secretbox_xsalsa20poly1305_KEYBYTES
 #define crypto_secretbox_NONCEBYTES crypto_secretbox_xsalsa20poly1305_NONCEBYTES
 #define crypto_secretbox_ZEROBYTES crypto_secretbox_xsalsa20poly1305_ZEROBYTES
@@ -188,27 +188,27 @@ extern int crypto_secretbox_xsalsa20poly1305_tweet_open(unsigned char *,const un
 #define crypto_secretbox_xsalsa20poly1305_BOXZEROBYTES crypto_secretbox_xsalsa20poly1305_tweet_BOXZEROBYTES
 #define crypto_secretbox_xsalsa20poly1305_VERSION crypto_secretbox_xsalsa20poly1305_tweet_VERSION
 #define crypto_secretbox_xsalsa20poly1305_IMPLEMENTATION "crypto_secretbox/xsalsa20poly1305/tweet"
-#define crypto_sign_PRIMITIVE "ed25519"
-#define crypto_sign crypto_sign_ed25519
-#define crypto_sign_open crypto_sign_ed25519_open
-#define crypto_sign_keypair crypto_sign_ed25519_keypair
-#define crypto_sign_BYTES crypto_sign_ed25519_BYTES
-#define crypto_sign_PUBLICKEYBYTES crypto_sign_ed25519_PUBLICKEYBYTES
-#define crypto_sign_SECRETKEYBYTES crypto_sign_ed25519_SECRETKEYBYTES
-#define crypto_sign_IMPLEMENTATION crypto_sign_ed25519_IMPLEMENTATION
-#define crypto_sign_VERSION crypto_sign_ed25519_VERSION
-#define crypto_sign_ed25519_tweet_BYTES 64
-#define crypto_sign_ed25519_tweet_PUBLICKEYBYTES 32
-#define crypto_sign_ed25519_tweet_SECRETKEYBYTES 64
-extern int crypto_sign_ed25519_tweet(unsigned char *,unsigned long long *,const unsigned char *,unsigned long long,const unsigned char *);
-extern int crypto_sign_ed25519_tweet_open(unsigned char *,unsigned long long *,const unsigned char *,unsigned long long,const unsigned char *);
-extern int crypto_sign_ed25519_tweet_keypair(unsigned char *,unsigned char *);
-#define crypto_sign_ed25519_tweet_VERSION "-"
-#define crypto_sign_ed25519_BYTES crypto_sign_ed25519_tweet_BYTES
-#define crypto_sign_ed25519_PUBLICKEYBYTES crypto_sign_ed25519_tweet_PUBLICKEYBYTES
-#define crypto_sign_ed25519_SECRETKEYBYTES crypto_sign_ed25519_tweet_SECRETKEYBYTES
-#define crypto_sign_ed25519_VERSION crypto_sign_ed25519_tweet_VERSION
-#define crypto_sign_ed25519_IMPLEMENTATION "crypto_sign/ed25519/tweet"
+#define crypto_sign_PRIMITIVE "edwards25519sha512batch"
+#define crypto_sign crypto_sign_edwards25519sha512batch_ref
+#define crypto_sign_open crypto_sign_edwards25519sha512batch_ref_open
+#define crypto_sign_keypair crypto_sign_edwards25519sha512batch_ref_keypair
+#define crypto_sign_BYTES crypto_sign_edwards25519sha512batch_BYTES
+#define crypto_sign_PUBLICKEYBYTES crypto_sign_edwards25519sha512batch_PUBLICKEYBYTES
+#define crypto_sign_SECRETKEYBYTES crypto_sign_edwards25519sha512batch_SECRETKEYBYTES
+#define crypto_sign_IMPLEMENTATION crypto_sign_edwards25519sha512batch_IMPLEMENTATION
+#define crypto_sign_VERSION crypto_sign_edwards25519sha512batch_VERSION
+#define crypto_sign_edwards25519sha512batch_tweet_BYTES 64
+#define crypto_sign_edwards25519sha512batch_tweet_PUBLICKEYBYTES 32
+#define crypto_sign_edwards25519sha512batch_tweet_SECRETKEYBYTES 64
+extern int crypto_sign_edwards25519sha512batch_tweet(unsigned char *,unsigned long long *,const unsigned char *,unsigned long long,const unsigned char *);
+extern int crypto_sign_edwards25519sha512batch_tweet_open(unsigned char *,unsigned long long *,const unsigned char *,unsigned long long,const unsigned char *);
+extern int crypto_sign_edwards25519sha512batch_tweet_keypair(unsigned char *,unsigned char *);
+#define crypto_sign_edwards25519sha512batch_tweet_VERSION "-"
+#define crypto_sign_edwards25519sha512batch_BYTES crypto_sign_edwards25519sha512batch_tweet_BYTES
+#define crypto_sign_edwards25519sha512batch_PUBLICKEYBYTES crypto_sign_edwards25519sha512batch_tweet_PUBLICKEYBYTES
+#define crypto_sign_edwards25519sha512batch_SECRETKEYBYTES crypto_sign_edwards25519sha512batch_tweet_SECRETKEYBYTES
+#define crypto_sign_edwards25519sha512batch_VERSION crypto_sign_edwards25519sha512batch_tweet_VERSION
+#define crypto_sign_edwards25519sha512batch_IMPLEMENTATION "crypto_sign/edwards25519sha512batch/tweet"
 #define crypto_stream_PRIMITIVE "xsalsa20"
 #define crypto_stream crypto_stream_xsalsa20
 #define crypto_stream_xor crypto_stream_xsalsa20_xor
@@ -240,6 +240,7 @@ extern int crypto_stream_salsa20_tweet_xor(unsigned char *,const unsigned char *
 #define crypto_stream_salsa20_IMPLEMENTATION "crypto_stream/salsa20/tweet"
 #define crypto_verify_PRIMITIVE "16"
 #define crypto_verify crypto_verify_16
+#define crypto_verify_16 crypto_verify_16_ref
 #define crypto_verify_BYTES crypto_verify_16_BYTES
 #define crypto_verify_IMPLEMENTATION crypto_verify_16_IMPLEMENTATION
 #define crypto_verify_VERSION crypto_verify_16_VERSION
@@ -255,4 +256,5 @@ extern int crypto_verify_32_tweet(const unsigned char *,const unsigned char *);
 #define crypto_verify_32_BYTES crypto_verify_32_tweet_BYTES
 #define crypto_verify_32_VERSION crypto_verify_32_tweet_VERSION
 #define crypto_verify_32_IMPLEMENTATION "crypto_verify/32/tweet"
+#define crypto_verify_32 crypto_verify_32_ref
 #endif
